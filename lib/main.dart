e71f3431d3f9e38b2324afcdc27ea9e86944f707
Ahmad <muahmad710@gmail.com>
2023-01-12 01:00:52 +0500

    Row and column functionalities implemented

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           ],
         ),
       title: "Test Responsive Framework",
-      home: const HomeScreen(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final ScrollController controller1 = ScrollController();
+  final ScrollController controller2 = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                height: 700,
-                width: 700,
-                color: Colors.red,
-                child: Image.asset('assets/images/rnd1.jpg', fit: BoxFit.fill),
+      body: Scrollbar(
+        controller: controller2,
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          controller: controller2,
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            controller: controller1,
+            child: Center(
+              child: ResponsiveRowColumn(
+                rowMainAxisAlignment: MainAxisAlignment.center,
+                rowPadding: const EdgeInsets.all(16),
+                columnPadding: const EdgeInsets.all(16),
+                layout: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP) ?ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+                children: [
+                  ResponsiveRowColumnItem(
+                    child: Container(
+                      height: 700,
+                      width: 700,
+                      color: Colors.red,
+                      child: Image.asset('assets/images/rnd1.jpg', fit: BoxFit.fill),
+                    ),
+                  ),
+                  ResponsiveRowColumnItem(
+                    child: Container(
+                      height: 700,
+                      width: 700,
+                      color: Colors.blue,
+                      child: Image.asset(
+                        'assets/images/rnd2.jpg',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  ResponsiveRowColumnItem(
+                    child: Container(
+                      height: 700,
+                      width: 700,
+                      color: Colors.yellow,
+                      child: Image.asset('assets/images/rnd3.jpg', fit: BoxFit.fill),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                height: 700,
-                width: 700,
-                color: Colors.blue,
-                child: Image.asset(
-                  'assets/images/rnd2.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Container(
-                height: 700,
-                width: 700,
-                color: Colors.yellow,
-                child: Image.asset('assets/images/rnd3.jpg', fit: BoxFit.fill),
-              ),
-            ],
+            ),
           ),
         ),
       ),
